4ffd4d12ea48b9b00f13396a51d743c2552bcb86
紫涯 <thkdog@hotmail.com>
2017-04-10 00:51:17 +0800

    add bench

--- ssr/index.js
+++ ssr/index.js
@@ -1,7 +1,20 @@
-const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
-const bundle = require('../dist-ssr/vue-ssr-bundle.json')
-var http = require('http');
+var bench = {
+  _startTime: null,
+  start() {
+    this._startTime = new Date();
+  },
+  end() {
+    var endTime = new Date();
+    var span = endTime - this._startTime;
+    console.log('\n... cost ' + span + 'ms');
+  }
+}
 
+const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
+const http = require('http');
+
+bench.start();
+const bundle = require('../dist-ssr/vue-ssr-bundle.json')
 const renderer = createBundleRenderer(bundle, {
   template:
     '<!DOCTYPE html>' +
@@ -16,7 +29,18 @@ const renderer = createBundleRenderer(bundle, {
         // context.state will be injected here
       '</body>' +
     '</html>'
-})
+});
+
+// var stream = renderer.renderToStream({ sampleProp: 'initial data' })
+// stream.pipe(process.stdout)
+// stream.on('end', function() {
+//   bench.end();
+// });
+
+renderer.renderToString({ sampleProp: 'initial data' }, function(err, html) {
+  console.log(html);
+  bench.end();
+});
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
