5c4337e4d24acd97334230d06f843117c2bb0778
紫涯 <thkdog@hotmail.com>
2017-04-10 01:13:39 +0800

    dump styles

--- src/main-ssr.js
+++ src/main-ssr.js
@@ -7,6 +7,9 @@ const app = new Vue(App)
 // the default export should be a function
 // which will receive the context of the render call
 export default context => {
+  console.log('<<<<<<<<<<<< props in context >>>>>>>>>>>>>>')
+  console.log(context.head)
+  console.log(context.styles)
   // data pre-fetching
   // return app.fetchServerData(context.url).then(() => {
   //   return app
